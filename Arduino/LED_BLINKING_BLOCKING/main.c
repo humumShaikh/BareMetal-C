0c416bd9b1b3a76d8193cb1cf9045ec8ed69f5da
CWIR3 <humumshaikh0@gmail.com>
2026-02-13 01:10:10 +0530

    Add files via upload

---
+++ Arduino/LED_BLINKING_BLOCKING/main.c
@@ -0,0 +1,20 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+
+  DDRB = DDRB | (1<<PB5);
+
+  while(1)
+  {
+    PORTB = PORTB | (1<<PB5);
+    _delay_ms(500);
+    PORTB = PORTB & (0<<PB5);
+    _delay_ms(500);
+  }
+
+  return 0;
+
+}
